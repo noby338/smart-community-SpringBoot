87a6e0e01e87dce7bb4ef624a4d09cbec2b8bb90
Fiver.hu <975417028@qq.com>
2022-07-04 10:43:26 +0800

    第一次提交

---
+++ smart_community/src/main/java/com/woniuxy/smart_community/SmartCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.woniuxy.smart_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCommunityApplication.class, args);
+    }
+
+}
