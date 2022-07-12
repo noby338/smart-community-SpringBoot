cd1822c6ed2b4e96b54f6a9b3139be35b9e6c1be
sun-computer <sun-computer@163.com>
2022-07-12 15:42:39 +0800

    孙浚钦：车辆信息dao、service、controller添加
    修改webMvcConfig添加路径映射

--- src/main/java/com/woniuxy/smart_community/dao/CarInfoDao.java
+++ src/main/java/com/woniuxy/smart_community/dao/CarInfoDao.java
@@ -8,34 +8,10 @@ import java.util.List;
 
 @Mapper
 public interface CarInfoDao {
-//    查询
+    //查询分页显示的车辆信息
     List<CarInfo> selectAllCarInfos();
-    CarInfo selectCarInfoByCarid(int carId);
-    CarInfo selectCarInfoByCarno(String carNo);
+    int selectCountCarInfo();
 
-//    添加
-
-    /**
-     * 添加车辆信息，默认添加carstate=1
-     * @param carInfo
-     */
-    void insertCarInfo(@Param("carInfo") CarInfo carInfo);
-
-//    修改
-    //void updateCarInfo(@Param("carInfo") CarInfo carInfo);
-
-    /**
-     * 基本功能改变车辆状态carstate
-     * @param carState
-     * @param carId
-     */
-    void updateCarInfoState(@Param("carState") int carState,@Param("carId") int carId);
-
-//    删除
-
-    /**
-     * 管理功能，真删除信息
-     * @param carId
-     */
-    void deleteCarInfoByCarid(int carId);
+    void updateImgByCarNumber(@Param("carNumber") String carNumber,
+                              @Param("imgAddress") String imgAddress);
 }
--- src/main/java/com/woniuxy/smart_community/service/impl/CarInfoServiceImpl.java
+++ src/main/java/com/woniuxy/smart_community/service/impl/CarInfoServiceImpl.java
@@ -1,11 +1,18 @@
 package com.woniuxy.smart_community.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.woniuxy.smart_community.dao.CarInfoDao;
+import com.woniuxy.smart_community.dao.ParkingInfoDao;
 import com.woniuxy.smart_community.entity.CarInfo;
+import com.woniuxy.smart_community.entity.ForSelect;
+import com.woniuxy.smart_community.entity.ParkingInfo;
 import com.woniuxy.smart_community.service.CarInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,10 +24,50 @@ import java.util.List;
 public class CarInfoServiceImpl implements CarInfoService {
     @Autowired
     CarInfoDao carInfoDao;
+    @Value("${view.carJpg}")
+    private String carJpgPre;
+
+    @Autowired
+    ParkingInfoDao parkingInfoDao;
 
     @Override
-    public List<CarInfo> getAllCarInfo() {
+    public PageInfo<CarInfo> getAllCarInfo(int pageIndex,int pageSize) {
+        PageHelper.startPage(pageIndex, pageSize);
         List<CarInfo> carInfos=carInfoDao.selectAllCarInfos();
-        return carInfos;
+        for(CarInfo carInfo:carInfos){
+            String carJpg = carInfo.getCarJpg();
+            if(carJpg!=null){
+                carInfo.setCarJpg(carJpgPre+carJpg);
+            }
+        }
+        PageInfo<CarInfo> pageInfo=new PageInfo<>(carInfos);
+        int total=carInfoDao.selectCountCarInfo();
+        pageInfo.setTotal(total);
+        return pageInfo;
+    }
+
+    @Override
+    public List<ForSelect> getALLParkingInfoIdAndParkNumber(int id) {
+        List<ForSelect> forSelects=new ArrayList<ForSelect>();
+        List<ParkingInfo> parkingInfos=parkingInfoDao.selectParkingInfoByParkingInfo(null);
+
+        for(ParkingInfo parkingInfo:parkingInfos){
+            ForSelect forSelect=new ForSelect();
+            forSelect.setValue(parkingInfo.getId());
+            forSelect.setLabel(parkingInfo.getParkNumber());
+            if(parkingInfo.getOwnersInfo()!=null){
+                forSelect.setDisabled(true);
+            }
+            if(parkingInfo.getId()==id){
+                forSelect.setLabel(parkingInfo.getParkNumber()+"(当前使用)");
+            }
+            forSelects.add(forSelect);
+        }
+        return forSelects;
+    }
+
+    @Override
+    public void addImgAddress(String carNumber, String imgAddress) {
+        carInfoDao.updateImgByCarNumber(carNumber,imgAddress);
     }
 }
